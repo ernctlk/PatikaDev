6f91630f5c324bd9910690dbc4175b717cf91938
ernctlk <102687949+ernctlk@users.noreply.github.com>
2024-11-11 23:32:32 +0300

    Add files via upload

---
+++ Faktoriyel Hesaplayan Program/src/Main.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan=new Scanner(System.in);
+        System.out.println("Faktoriyel Sayısı : ");
+        int n = scan.nextInt();
+        int total =1;
+
+        for (int i = 1 ; i<=n ; i++) {
+            total = total * i;
+        }
+        System.out.println(n + " Faktoriyel : " + total);
+
+    }
+}
