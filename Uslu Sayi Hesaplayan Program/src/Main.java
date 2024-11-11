6927f1597217f27f6c4cf1fd1e24ae04bb1aa98f
ernctlk <102687949+ernctlk@users.noreply.github.com>
2024-11-11 23:46:43 +0300

    Add files via upload

---
+++ Uslu Sayi Hesaplayan Program/src/Main.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        int n , k ;
+        Scanner input = new Scanner(System.in);
+        System.out.println("Üstü Alınacak Sayı : ");
+        n = input.nextInt();
+        System.out.println("Üs Olacak Sayı : ");
+        k= input.nextInt();
+        int total=1 , i = 1;
+        while (i<= k){
+            total*= n;
+            i++;
+
+
+        }
+        System.out.println("Cevap: "+total);
+    }
+}
